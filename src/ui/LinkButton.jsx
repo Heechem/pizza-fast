e7af21208809a71c701937f31263a359ad13dbf8
Heechem <hichempeter@gmail.com>
2023-07-25 08:25:58 -0600

    style error Link button

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,11 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LinkButton = ({ children, to }) => {
+  const navigate = useNavigate();
+  const className = " text-sm text-blue-500 hover:text-lg  hover:text-blue-600";
+
+  if (to === "-1")
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
   return (
-    <Link
-      to={to}
-      className=" text-sm text-blue-500 hover:text-lg  hover:text-blue-600"
-    >
+    <Link to={to} className={className}>
       {children}
     </Link>
   );
