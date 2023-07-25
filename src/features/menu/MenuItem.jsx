456f685829cbca37c48223efecc7cdec633722f9
Heechem <hichempeter@gmail.com>
2023-07-25 13:55:40 -0600

    Add new font and style menu

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,17 +1,14 @@
-import { formatCurrency } from '../../utilities/helpers';
+import { formatCurrency } from "../../utilities/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img
-        src={imageUrl}
-        alt={name}
-      />
+    <li className="flex gap-4">
+      <img src={imageUrl} alt={name} className="h-24" />
       <div>
         <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
+        <p>{ingredients.join(", ")}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
